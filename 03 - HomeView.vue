4d89bc00e5971b9bd66e7f7ae0e4400b1045bda9
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2025-01-12 21:51:12 -0500

    Remove duplicated App.vue file and addition of the HomeView file

---
+++ 03 - HomeView.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <header class="py-10">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
+      </div>
+    </header>
+
+    <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+      <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+        <!-- Add onchain metrics here -->
+      </div>
+    </div>
+
+    <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-1">
+        <div class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+          <div class="flex w-full items-center justify-between space-x-6 p-6">
+            <div class="flex-1 truncate">
+              <div class="flex items-center space-x-3">
+                <!-- Add transactions here -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+      <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
+        <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+          <div class="flex w-full items-center justify-between space-x-6 p-6">
+            <div class="flex-1 truncate">
+              <div class="flex items-center space-x-3 overflow-x-scroll scrollbar">
+                <!-- Add blocks here -->
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
